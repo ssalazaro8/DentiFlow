58df4164053928b923d0d05e08cd7cad9a60e60d
Elduque77 <jfduqueg1@eafit.edu.co>
2026-08-05 12:06:25 +0000

    Refactor LaboratoriesConfig name and add initial migration for Laboratory model

--- apps/laboratories/apps.py
+++ apps/laboratories/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LaboratoriesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "laboratories"
+    name = "apps.laboratories"
