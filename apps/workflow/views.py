4bab974dc057047b070c6f51da23ca4c17241702
Daniel Villegas <dvillegas2@eafit.edu.co>
2026-08-04 22:40:39 -0500

    feat: implement workflow module

--- apps/workflow/views.py
+++ apps/workflow/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import WorkflowForm
+
+from .services import WorkflowService
+
+
+def workflow_list(request):
+
+    workflows = WorkflowService.get_all()
+
+    return render(
+        request,
+        "workflow/list.html",
+        {
+            "workflows": workflows
+        }
+    )
+
+
+def workflow_create(request):
+
+    form = WorkflowForm()
+
+    return render(
+        request,
+        "workflow/create.html",
+        {
+            "form": form
+        }
+    )
