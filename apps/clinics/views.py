5924e50fab74212306f99958e578211da2a73f78
SamCr17 <santimeca1221@gmail.com>
2026-08-09 23:39:39 -0500

    FR-04: implement dental clinic management (model, forms, views, templates)

--- apps/clinics/views.py
+++ apps/clinics/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ClinicForm
 from .services import ClinicService
 
 
 def clinic_list(request):
-
     clinics = ClinicService.get_all()
-
     return render(
         request,
         "clinics/list.html",
@@ -17,9 +15,13 @@ def clinic_list(request):
 
 
 def clinic_create(request):
-
-    form = ClinicForm()
-
+    if request.method == "POST":
+        form = ClinicForm(request.POST)
+        if form.is_valid():
+            ClinicService.create(form.cleaned_data)
+            return redirect("clinic_list")
+    else:
+        form = ClinicForm()
     return render(
         request,
         "clinics/create.html",
