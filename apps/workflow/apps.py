03fbd44af993065d7af3b2c4de5859d415e24df2
SamCr17 <santimeca1221@gmail.com>
2026-08-11 10:38:55 -0500

    Merge dev into origin/feature/user-roles-fr16

--- apps/workflow/apps.py
+++ apps/workflow/apps.py
@@ -2,12 +2,5 @@ from django.apps import AppConfig
 
 
 class WorkflowConfig(AppConfig):
-<<<<<<< HEAD
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.workflow'
-=======
-
-    default_auto_field = "django.db.models.BigAutoField"
-
-    name = "apps.workflow"
->>>>>>> origin/dev
--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,18 @@
-<<<<<<< HEAD
 """
 URL configuration for config project.
 """
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('clinics/', include('apps.clinics.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('laboratories/', include('apps.laboratories.urls')),
-=======
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-
+    path('admin/', admin.site.urls),
+    path('clinics/', include('apps.clinics.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('laboratories/', include('apps.laboratories.urls')),
     path(
         "admin/",
         admin.site.urls,
@@ -29,7 +24,6 @@ urlpatterns = [
             "apps.dental_cases.urls"
         ),
     ),
->>>>>>> origin/dev
 ]
 
 
